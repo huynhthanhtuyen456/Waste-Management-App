23a7b7482ba75b58a48c06901e5bc11d6ad44068
Tuyen Huynh <huynhthanhtuyen456@gmail.com>
2024-10-28 14:39:47 +0700

    Organize bigger application following FastAPI standard

--- app/main.py
+++ app/main.py
@@ -1,10 +1,29 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+
+from app.dependencies import get_query_token, get_token_header
+from app.internal import admin
+from app.routes import items, users
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
 
 
-# Initialize the FastAPI app
-app = FastAPI()
+app.include_router(
+    users.router,
+    prefix="/api/v1",
+)
+app.include_router(
+    items.router,
+    prefix="/api/v1"
+)
+app.include_router(
+    admin.router,
+    prefix="/api/v1/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}},
+)
 
 
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to the Waste Management App"}
+async def root():
+    return {"message": "Hello Bigger Applications!"}
