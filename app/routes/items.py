13d3af6c6cd4c8ac1533fe7c834079a655ebfde2
Tuyen Huynh <huynhthanhtuyen456@gmail.com>
2024-10-29 17:52:00 +0700

    Structure base source

--- app/routes/items.py
+++ app/routes/items.py
@@ -6,7 +6,10 @@ router = APIRouter(
     prefix="/items",
     tags=["items"],
     dependencies=[Depends(get_token_header)],
-    responses={404: {"description": "Not found"}},
+    responses={
+        404: {"description": "Not found"},
+        403: {"description": "Forbidden"},
+    },
 )
 
 
